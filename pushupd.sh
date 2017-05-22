833614ac4e978c19ab791bf210102ffa762fb8b9
teamthevibe <team.thevibe@gmail.com>
2017-05-22 16:58:20 +0300

    update push in : Mon May 22 16:58:20 IDT 2017

---
+++ pushupd.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+cd /opt/mcfly
+#git pull origin master
+git pull
+/usr/local/bin/python3.4  new_xml_gen.py
+sleep 7
+git add *
+git commit -a -m "update push in : `date`"
+git push 
+#sleep 120
+##!wget http://repo.the-vibe.co.il/Service
+#content=$(wget "http://repo.the-vibe.co.il/Service" -q -O -)
+#wget  http://repo.the-vibe.co.il/Service >/dev/null &
